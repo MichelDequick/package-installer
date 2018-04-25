60f4f8fbffebd8b01854e36f8246b75e24f4e885
Michel Dequick <michel.dequick@gmail.com>
2018-04-25 09:41:29 +0200

    UI update

--- package-installer.sh
+++ package-installer.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+# Font: http://patorjk.com/software/taag/#p=display&f=Slant&t=PkgInstaller
+
+echo """
+echo "    ____  __         ____           __        ____         "
+echo "   / __ \/ /______ _/  _/___  _____/ /_____ _/ / /__  _____"
+echo "  / /_/ / //_/ __ `// // __ \/ ___/ __/ __ `/ / / _ \/ ___/"
+echo " / ____/ ,< / /_/ // // / / (__  ) /_/ /_/ / / /  __/ /    "
+echo "/_/   /_/|_|\__, /___/_/ /_/____/\__/\__,_/_/_/\___/_/     "
+echo "           /____/                                          "
+
 # Check if first argument is given
 if [ -z "$1" ]
     then
-        echo "Please specify package file"
+        echo "◈ Please specify package file"
     else
 
     # Update source list
@@ -20,4 +30,4 @@ if [ -z "$1" ]
     done
 fi
 
-echo "Done"
+echo "◇ Done"
